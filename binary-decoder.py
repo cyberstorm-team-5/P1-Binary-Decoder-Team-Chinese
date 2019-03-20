31b7f9582d8ab808bc8be036750ea1ac898c22a6
Andrew Maurice <andromulin@gmail.com>
2019-03-20 09:47:18 -0500

    added github repo link to binary-decoder.py

--- binary-decoder.py
+++ binary-decoder.py
@@ -1,13 +1,15 @@
 ################################################################
 # Authors: Team Chinese (Lane Arnold, Christopher Boquet,
-# 	   Christopher Bouton, Darrell Durousseaux, Clay Fonseca,
-#	   Rebecca Grantham, Andrew Maurice)
+# 	       Christopher Bouton, Darrell Durousseaux, Clay Fonseca,
+#	         Rebecca Grantham, Andrew Maurice)
 # Class: CSC 442
 # Date: 3-29-2019
-# Description: The Python code will be able to take in binary
-#	       input files (7- or 8-bit ASCII) containing only
-#	       printable charactersthrough stdin and send the
-#	       decoded message to stdout.
+# Github repo: https://github.com/cyberstorm-team-5/P1-Binary-Decoder-Team-Chinese
+# Description: Program 1: Binary Decoder
+#              The Python code will be able to take in binary
+#	             input files (7- or 8-bit ASCII) containing only
+#	             printable charactersthrough stdin and send the
+#	             decoded message to stdout.
 ################################################################
 
 import sys
